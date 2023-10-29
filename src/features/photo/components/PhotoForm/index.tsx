ea8e1f4cc9c34fb70904314ecd54bd4fb12814a6
Phước Phú <phuocphu414141@gmail.com>
2023-10-29 11:21:15 +0700

    .

--- src/features/photo/components/PhotoForm/index.tsx
+++ src/features/photo/components/PhotoForm/index.tsx
@@ -2,14 +2,23 @@ import Styles from './PhotoForm.module.scss'
 import Select from 'react-select';
 import { PHOTO_CATEGORY_OPTIONS } from '../../../../constants/global';
 import { useForm, Controller } from 'react-hook-form';
+import axios from 'axios';
+import { ReactElement, useState } from 'react';
 type Props = {}
 
 interface IFormInput {
     title: string;
     category: {};
+    photo: string;
 }
 export default function PhotoForm({ }: Props) {
     const { register, handleSubmit, control, formState: { errors }, } = useForm<IFormInput>();
+    const [imgShow, setImgShow] = useState(``)
+    const handleClickRandom = async () => {
+        let id = Math.floor(Math.random() * 2000)
+        setImgShow(`https://picsum.photos/id/${id}/300/300`)
+        // console.log(imgShow)
+    }
     return (
         <form className={Styles.PhotoForm} onSubmit={handleSubmit((data) => console.log(data))}>
             <div className="form-group">
@@ -41,16 +50,16 @@ export default function PhotoForm({ }: Props) {
             <div className="form-group">
                 <label htmlFor=''>Photo</label>
                 <div>
-                    <button>
+                    <button onClick={handleClickRandom}   >
                         Random a photo
                     </button>
                 </div>
                 <div>
-                    <img src='' />
+                    <img src={imgShow} alt='Oops...Cant find image'{...register('photo', { required: true })} />
                 </div>
             </div>
             <div className="form-group">
-                <button type="submit" className="btn btn-primary">Add to album</button>
+                <button type="submit" className="btn btn-primary" >Add to album</button>
             </div>
         </form>
     )
